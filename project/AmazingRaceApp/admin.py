0e30c1e598d25c045d44ed4ddcb9227af9f8c244
mful7998 <mohammed.fulwala@sydney.edu.au>
2019-10-02 15:38:10 +1000

    Fixed all ERD models

--- project/AmazingRaceApp/admin.py
+++ project/AmazingRaceApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import ProfilePictures, Game, GameCreator, GamePlayer, \
+    Location, LocationGame, LocationUser
 
 # Register your models here.
+
+admin.site.register(Game)
+admin.site.register(GamePlayer)
+admin.site.register(GameCreator)
+admin.site.register(LocationUser)
+admin.site.register(Location)
+admin.site.register(LocationGame)
+admin.site.register(ProfilePictures)
