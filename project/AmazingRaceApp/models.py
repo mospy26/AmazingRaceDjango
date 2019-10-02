568c2f31ee9b4a5e64771d2bbf2dcfa08f424269
mful7998 <mohammed.fulwala@sydney.edu.au>
2019-10-02 14:06:15 +1000

    Profile picture ERD implemented

--- project/AmazingRaceApp/models.py
+++ project/AmazingRaceApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class ProfilePictures(models.Model):
+    picture = models.ImageField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
