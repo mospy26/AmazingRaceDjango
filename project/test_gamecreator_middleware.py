809f91f9a91bc3b37ea7463d4bfaf63d25e96d85
Brendon <blam6948@uni.sydney.edu.au>
2019-10-03 22:42:48 +1000

    Near Completion of GamePlayerMiddleware

--- project/test_gamecreator_middleware.py
+++
@@ -1,7 +0,0 @@
-from AmazingRaceApp.api.GameCreatorMiddleware import GameCreatorMiddleware
-
-g = GameCreatorMiddleware("blam")
-games = g.get_created_games()
-
-for game in games:
-    print(game.code)
