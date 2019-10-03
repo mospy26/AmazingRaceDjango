076b3aead2489efc6ea88cf4e9d03eb143dab189
Brendon <blam6948@uni.sydney.edu.au>
2019-10-03 14:25:30 +1000

    Updated Method names

--- project/AmazingRaceApp/api/LocationMiddleware.py
+++ project/AmazingRaceApp/api/LocationMiddleware.py
@@ -8,28 +8,28 @@ class LocationMiddleware:
 
     # Returns a list of location name
     @classmethod
-    def getAllLocations(cls, game): 
+    def get_all_locations(cls, game): 
         return Location.objects.values('name', game=game)
     
     # Returns the game associated with the location 
     @classmethod
-    def getGame(cls, location):
+    def get_game(cls, location):
         return Location.objects.values('game', location=location)
 
     # Returns the clues associated with the location 
     @classmethod
-    def getClues(cls, location): 
+    def get_clues(cls, location): 
         return Location.objects.values('clues', location=location)
     
     @classmethod
-    def getLongitude(cls, location): 
+    def get_longitude(cls, location): 
         return Location.objects.values('longitude', location=location)
     
     @classmethod
-    def getLatitude(cls, location): 
+    def get_latitude(cls, location): 
         return Location.objects.values('latitude', location=location)
 
     @classmethod
-    def getUsersInLocation(cls, location): 
+    def get_users_in_location(cls, location): 
         return LocationUser.objects.filter(location=location) 
     
--- project/AmazingRaceApp/api/UserMiddleware.py
+++ project/AmazingRaceApp/api/UserMiddleware.py
@@ -8,20 +8,20 @@ class UserMiddleware:
 
     # TODO: # https://stackoverflow.com/questions/9498012/how-to-display-images-from-model-in-django
     @classmethod
-    def getProfilePicture(cls, user): 
+    def get_profile_picture(cls, user): 
         return ProfilePictures.objects.filter(user=user)
 
     # Returns all the games created by the user 
     @classmethod
-    def getAllGamesCreated(cls, user): 
+    def get_all_games_created(cls, user): 
         return GameCreator.objects.filter(creator=user)
 
     # Returns all the games that the user is participating
     @classmethod
-    def getAllGamesParticipating(cls, user): 
+    def get_all_games_participating(cls, user): 
         return GamePlayer.objects.values('game', player=user)
     
     # Returns the ranks, correspondent to the game of a user 
     @classmethod
-    def getRankOfPlayer(cls, user): 
+    def get_rank_of_player(cls, user): 
         return GamePlayer.objects.values('game', 'rank', player=user)
