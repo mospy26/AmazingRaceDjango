39f87482fc0d8b584307f47bf8fcdeb03f4fb579
mful7998 <mohammed.fulwala@sydney.edu.au>
2019-10-12 17:49:28 +1100

    Login required mixin added for demonstration

--- project/AmazingRaceApp/urls.py
+++ project/AmazingRaceApp/urls.py
@@ -4,4 +4,5 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.Test.as_view(), name='profilepage'),
+    path('login/', views.Login.as_view(), name='login')
 ]
