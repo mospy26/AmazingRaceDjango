793990d240fe73d19de0cd169cccad7a5155e81f
mful7998 <mohammed.fulwala@sydney.edu.au>
2019-10-12 16:17:47 +1100

    Demo of how templating works

--- project/AmazingRaceApp/views.py
+++ project/AmazingRaceApp/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import generic
 
+from AmazingRaceApp.api.GamePlayerMiddleware import GamePlayerMiddleware
+
 
 class Test(generic.TemplateView):
 
     template_name = 'profilepage.html'
+
+    def get(self, request):
+
+        g = GamePlayerMiddleware(username='echa')
+        return render(self.request, self.template_name, context={
+            'name': g.user.first_name
+        })
