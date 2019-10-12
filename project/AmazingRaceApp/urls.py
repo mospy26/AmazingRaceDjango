ca161b04785c46c4d915d20766096a381e81da9c
mful7998 <mohammed.fulwala@sydney.edu.au>
2019-10-12 15:23:42 +1100

    Views and urls added

---
+++ project/AmazingRaceApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('', views.Test.as_view(), name='profilepage'),
+]
