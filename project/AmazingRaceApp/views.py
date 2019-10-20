f51db51f84cc28e4f13a6df7a36be7eced1ac597
mful7998 <mful7998@uni.sydney.edu.au>
2019-10-20 14:42:53 +1100

    Views fixed

--- project/AmazingRaceApp/views.py
+++ project/AmazingRaceApp/views.py
@@ -8,17 +8,6 @@ from django.views import generic
 from AmazingRaceApp.api.GamePlayerMiddleware import GamePlayerMiddleware
 
 
-class Test(LoginRequiredMixin, generic.TemplateView):
-    template_name = 'profilepage.html'
-    login_url = '/login'
-
-    def get(self, request):
-        g = GamePlayerMiddleware(username='echa')
-        return render(self.request, self.template_name, context={
-            'user': g.user
-        })
-
-
 class Login(generic.TemplateView):
     template_name = 'topbar.html'
 
