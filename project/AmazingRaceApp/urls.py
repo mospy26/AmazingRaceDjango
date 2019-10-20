1e092c2a0bc8d7fbcd71546bfbffca9d03905003
mful7998 <mful7998@uni.sydney.edu.au>
2019-10-20 17:59:35 +1100

    Urls fixed

--- project/AmazingRaceApp/urls.py
+++ project/AmazingRaceApp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('user/', views.ProfilepageView.as_view(), name='user'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='login.html'), name='logout'),
-    path('register/', views.RegisterView.as_view(), name='user'),
-    path('user/created/', views.GameCreatedListView.as_view(), name='user'),
-    path('user/played/', views.GamePlayedListView.as_view(), name='user'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('user/created/', views.GameCreatedListView.as_view(), name='created'),
+    path('user/played/', views.GamePlayedListView.as_view(), name='played'),
 ]
