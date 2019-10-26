58187bf79e23d35a9e1a64661eea704658713e4e
kesu6572 <kesu6572@uni.sydney.edu.au>
2019-10-26 15:25:56 +1100

    Added game name and id as well as search button to add location

--- project/AmazingRaceApp/views.py
+++ project/AmazingRaceApp/views.py
@@ -185,7 +185,10 @@ class LocationAdd(LoginRequiredMixin, generic.TemplateView):
     login_url = '/login'
     
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        self.game = _GameMiddleware('LQGY-M42U')
+        return render(request, self.template_name, context={
+            'game_details': self.game.get_code_and_name()
+        })
 
 class GameSettings(LoginRequiredMixin, generic.TemplateView):
     template_name = 'gamesetting.html'
