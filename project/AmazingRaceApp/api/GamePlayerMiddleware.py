4c45307cf4f5eb5b1cd1afb8f631885b4d4bf1fb
mful7998 <mohammed.fulwala@sydney.edu.au>
2019-10-26 21:33:30 +1100

    List game played fixed

--- project/AmazingRaceApp/api/GamePlayerMiddleware.py
+++ project/AmazingRaceApp/api/GamePlayerMiddleware.py
@@ -4,6 +4,8 @@ from django.core.files import File
 
 import traceback
 import os
+
+from AmazingRaceApp.api.GameMiddleware import _GameMiddleware
 from ..models import ProfilePictures, GamePlayer, Game, Location, LocationUser, GameCreator
 
 
@@ -196,5 +198,5 @@ class GamePlayerMiddleware:
         return no_of_visited_locations, total_locations
 
     def get_status_of_game(self, game_code):
-        game = Game.objects.get(code=game_code)
-        return game.live
+        game = _GameMiddleware(game_code)
+        return game.get_status()
