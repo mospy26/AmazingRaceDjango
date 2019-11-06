78560f3ff314d53b2b573a04b88a63db3ac42322
Kevin Su <kevinsu@outlook.co.nz>
2019-11-06 19:14:57 +1100

    Testing for user ability to join various games (valid and invalid)

--- project/AmazingRaceApp/tests/parent_tests.py
+++ project/AmazingRaceApp/tests/parent_tests.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
-
+from AmazingRaceApp.models import GameCreator, Game
+from django.db.models import Subquery
 
 class DatabaseRequiredTests(TestCase):
 
@@ -12,3 +13,18 @@ class DatabaseRequiredTests(TestCase):
         'fixtures_location_test',
         'fixtures_locationuser_test'
     ]
+
+    def setUp(self):
+        self.init_david()
+        self.init_mustafa()
+
+    def init_david(self):
+        self.david = User.objects.get(first_name="David")
+        self.david_game_creator = GameCreator.objects.filter(creator = self.david).select_related('game')
+        # self.david_created_games = Game.objects.filter(id__in=Subquery(self.david_game_creator.values('game')))
+        self.david_created_games = self.david_game_creator.values('game')
+
+    def init_mustafa(self):
+        self.mustafa = User.objects.get(first_name="Mustafa")
+        self.mustafa_game_creator = GameCreator.objects.filter(creator = self.mustafa).select_related('game')
+        self.mustafa_created_games = self.mustafa_game_creator.values('game')
--- project/AmazingRaceApp/tests/test_backend.py
+++ project/AmazingRaceApp/tests/test_backend.py
@@ -5,6 +5,4 @@ from AmazingRaceApp.tests.parent_tests import DatabaseRequiredTests
 
 class BackendUnitTests(DatabaseRequiredTests):
 
-    def test_example(self):
-        user = User.objects.get(username="40wam")
-        self.assertEquals(user.username, "40wam", "Incorrect username")
+    pass
