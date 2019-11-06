d844b9f3f5a4356bf53df7ccdd65ad8f0879f15b
mful7998 <mohammed.fulwala@sydney.edu.au>
2019-11-06 16:50:13 +1100

    New data added 2

--- project/AmazingRaceApp/tests/parent_tests.py
+++ project/AmazingRaceApp/tests/parent_tests.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
-from .factory_boy import UserFactory
 
 
 class DatabaseRequiredTests(TestCase):
 
     def setUp(self):
-        self.user1 = UserFactory(username="blam", password="blam")
+        pass
